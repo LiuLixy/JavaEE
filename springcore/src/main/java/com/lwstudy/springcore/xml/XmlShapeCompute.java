d38980990df663b2b57e5a7207b6ff8bfe9e292a
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-07-31 21:37:22 +0800

    XmlShapeCompute.java
    创建一个XmlShapeCompute 类，通过传入不同参数，打印输出图形的面积，边长信息

---
+++ springcore/src/main/java/com/lwstudy/springcore/xml/XmlShapeCompute.java
@@ -0,0 +1,44 @@
+package com.lwstudy.springcore.xml;
+
+import com.lwstudy.springcore.common.Shape;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/7/31 15:50
+ */
+
+
+public class XmlShapeCompute {
+    private Shape circular;
+    private Shape rectangle;
+    private Shape triangle;
+
+    public Shape computeShape(String shapeName) {
+        if (shapeName == null || shapeName.length() == 0) {
+            throw new IllegalArgumentException("Not found" + shapeName);
+        }
+        if (shapeName.equals("circular")) {
+            return circular;
+        }
+        if (shapeName.equals("rectangle")) {
+            return rectangle;
+        }
+        if (shapeName.equals("triangle")) {
+            return triangle;
+        } else {
+            throw new IllegalArgumentException("Not found" + shapeName);
+        }
+    }
+
+    public void setCircular(Shape circular) {
+        this.circular = circular;
+    }
+
+    public void setRectangle(Shape rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void setTriangle(Shape triangle) {
+        this.triangle = triangle;
+    }
+}
