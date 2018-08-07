6dd08087caa5eac95851853ce5ea68cede18fc7d
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-08-07 19:24:11 +0800

    SpEL基于XML配置

---
+++ springcore-SpEL/src/main/java/com/lwstudy/springcore/spel/GuessNumber.java
@@ -0,0 +1,13 @@
+package com.lwstudy.springcore.spel;
+
+import lombok.Data;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/8/7 18:27
+ * @Description SpEL基于XML的配置
+ */
+@Data
+public class GuessNumber {
+    private double number;
+}
