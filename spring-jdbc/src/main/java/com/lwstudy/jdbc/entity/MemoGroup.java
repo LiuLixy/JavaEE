ec621a0d8a2be513d93c3b1957e08c84440ffac4
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-08-23 21:11:55 +0800

    实体层

---
+++ spring-jdbc/src/main/java/com/lwstudy/jdbc/entity/MemoGroup.java
@@ -0,0 +1,58 @@
+package com.lwstudy.jdbc.entity;
+
+import java.util.Date;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/8/7 15:27
+ */
+public class MemoGroup {
+    
+    private int id;
+    private String name;
+    private Date createdTime;
+    private Date modifyTime;
+    
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+    
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+    
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+    
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+    
+    @Override
+    public String toString() {
+        return "MemoGroup{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createdTime=" + createdTime +
+                ", modifyTime=" + modifyTime +
+                '}';
+    }
+}
