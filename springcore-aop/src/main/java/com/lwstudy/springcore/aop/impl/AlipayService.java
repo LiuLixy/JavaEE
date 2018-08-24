55c532b96153292d0a0293a7142209252ea23f8f
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-08-24 15:48:27 +0800

    支付方式

---
+++ springcore-aop/src/main/java/com/lwstudy/springcore/aop/impl/AlipayService.java
@@ -0,0 +1,28 @@
+package com.lwstudy.springcore.aop.impl;
+
+import com.lwstudy.springcore.aop.PayService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/8/24 9:52
+ */
+@Service
+public class AlipayService implements PayService {
+
+    @Override
+    public void pay() {
+
+        // 1. 安全检查
+
+        // 2. 日志记录
+
+        // 3-A. 支付开始时间
+
+        // Alipay-核心支付逻辑，扣钱
+
+        // 3-B. 支付结束时间
+
+        System.out.println("AlipayService");
+    }
+}
---
+++ springcore-aop/src/main/java/com/lwstudy/springcore/aop/impl/BankService.java
@@ -0,0 +1,13 @@
+package com.lwstudy.springcore.aop.impl;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/8/24 11:43
+ */
+public class BankService {
+
+    public void pay() {
+        System.out.println("BankService");
+    }
+
+}
