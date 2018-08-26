830dcdaaf3abfdb83505ef96b7be1033a160a7f8
LiuLixy <37731358+LiuLixy@users.noreply.github.com>
2018-08-26 23:13:02 +0800

    Book实体类

---
+++ springcore-jdbc/src/main/java/com/lwstudy/springcore/jdbc/Book.java
@@ -0,0 +1,17 @@
+package com.lwstudy.springcore.jdbc;
+
+import lombok.Data;
+
+/**
+ * @Author: LiuWang
+ * @Created: 2018/8/25 11:31
+ */
+@Data
+public class Book {
+
+    private String bookName;
+
+    private String bookAuthor;
+
+    private String bookIsbn;
+}
